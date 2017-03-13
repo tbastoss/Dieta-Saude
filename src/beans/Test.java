bbb4d73d761d1a78ab7195386b305fe1d9ee5fb8
wallsmanBR <alves_thiagobastos@hotmail.com>
2017-03-13 19:32:49 -0300

    Atualização em Pessoa e Paciente

---
+++ src/beans/Test.java
@@ -0,0 +1,16 @@
+package beans;
+
+import java.util.Calendar;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calendar data = Calendar.getInstance();
+		int ano = data.get(Calendar.YEAR);
+		System.out.println(ano);
+		
+		
+	}
+
+}
